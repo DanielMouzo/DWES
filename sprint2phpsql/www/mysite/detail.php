ddaf1660bf04394ae1ca50a5d4844b356bd31887
Daniel Mouzo <dmouzo22@fpcoruna.afundacion.org>
2023-10-17 14:48:14 +0200

    Ejercicio 4 PHP y SQL

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -30,6 +30,12 @@
 
 	mysqli_close($db);
 ?>
+<p>Deja un nuevo comentario:</p>
+<form action="/comment.php" method="post">
+	<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+	<input type="hidden" name="pelicula_id" value="<?php echo $cancion_id; ?>">
+	<input type="submit" value="Comentar">
+</form>
 </ul>
 </body>
 </html>
