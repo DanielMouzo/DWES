1b7758d24bea4c7c86e4349a2ed92428c7a94b32
Daniel Mouzo <dmouzo22@fpcoruna.afundacion.org>
2023-10-18 14:07:39 +0200

    Ejercicio 5 PHP y SQL

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -33,7 +33,7 @@
 <p>Deja un nuevo comentario:</p>
 <form action="/comment.php" method="post">
 	<textarea rows="4" cols="50" name="new_comment"></textarea><br>
-	<input type="hidden" name="pelicula_id" value="<?php echo $cancion_id; ?>">
+	<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 	<input type="submit" value="Comentar">
 </form>
 </ul>
